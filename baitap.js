b063cef6aeb625084ed861cec9816dd8e689a2f2
phuocnguyendn96 <phuocnguyendn96@gmail.com>
2023-08-03 19:13:07 +0700

    upload

---
+++ baitap.js
@@ -0,0 +1,88 @@
+var body = document.querySelector("#body")
+var headerTong = document.querySelector(".header")
+var headerLogo = document.querySelector(".header-logo")
+var headerLogoImg = document.querySelector(".header-img")
+var headerMenu = document.querySelector(".header-menu")
+var carousel = document.querySelector(".carousel")
+var activity = document.querySelector(".activity")
+var activityImg = document.querySelector(".activity-img")
+var list = document.querySelector(".list")
+var listItemMot = document.querySelector("#list-item-mot")
+var listItemHai = document.querySelector("#list-item-hai")
+var listItemBa = document.querySelector("#list-item-ba")
+var listItemBon = document.querySelector("#list-item-bon")
+var listLogoMot = document.querySelector("#list-logo-mot")
+var listLogoHai = document.querySelector("#list-logo-hai")
+var listLogoBa = document.querySelector("#list-logo-ba")
+var listLogoBon = document.querySelector("#list-logo-bon")
+var logoImg = document.querySelector(".logo-img")
+var listContent = document.querySelector(".list-content")
+var viewAll = document.querySelector(".viewall")
+var viewAllButton = document.querySelector(".viewall-button")
+var download = document.querySelector(".download")
+// var downloadImg = document.querySelector(".download-img")
+// var downloadContent = document.querySelector(".download-content")
+// var contentText = document.querySelector.apply(".content-text")
+const contentLogo = document.querySelector(".content-logo")
+// var contentLogoDownload = document.querySelector(".logo-download")
+// var downloadSocial = document.querySelector(".socical-media")
+const contentTheI = contentLogo.getElementsByTagName("i")
+
+
+
+body.style.backgroundColor = "bisque"
+body.style.padding = "0"
+body.style.margin = "0"
+body.style.boxSizing = "border-box"
+
+headerTong.style.backgroundColor = "bisque"
+headerTong.style.paddingTop = "10px"
+headerTong.style.paddingBottom = "10px"
+headerTong.style.paddingLeft = "25px"
+
+headerLogo.style.marginBottom = "20px"
+
+carousel.classList.add("carousel-ne")
+
+activity.style.display = "flex"
+activity.style.flexDirection = "column"
+activity.style.justifyContent = "center"
+activity.style.alignItems = "center"
+activity.style.fontSize = "15px"
+activity.style.marginTop = "20px"
+
+activityImg.style.width = "70px"
+
+list.classList.add("listne")
+listItemMot.classList.add("listitem")
+listItemHai.classList.add("listitem")
+listItemBa.classList.add("listitem")
+listItemBon.classList.add("listitem")
+listLogoMot.classList.add("list-logo")
+listLogoHai.classList.add("list-logo")
+listLogoBa.classList.add("list-logo")
+listLogoBon.classList.add("list-logo")
+logoImg.classList.add("logo-img")
+listContent.classList.add("list-content")
+
+viewAll.classList.add("viewalll")
+viewAllButton.classList.add("viewalll-button")
+
+download.classList.add("downloadd")
+contentLogo.classList.add("content-logoo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
